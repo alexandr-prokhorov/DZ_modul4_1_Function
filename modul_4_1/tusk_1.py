4e426552dc572c2d34b60ef3c4efe0313fd29904
alexandr-prokhorov <prohorovalex0013@gmail.ru>
2024-11-04 21:32:46 +0400

    Выполнил первое задание. Completed tusk1

---
+++ modul_4_1/tusk_1.py
@@ -0,0 +1,17 @@
+# Напишите функцию, которая отображает на экран
+# форматированный текст, указанный ниже:
+# “Don't compare yourself with anyone in this world…
+# if you do so, you are insulting yourself.”
+#  Bill Gates
+
+
+def display_text():
+    name_autor = "Bill Gates"
+    print("“Don't compare yourself with anyone in this world…")
+    print("if you do so, you are insulting yourself.”")
+    print(f"{name_autor:>50}")
+
+display_text()
+
+
+
