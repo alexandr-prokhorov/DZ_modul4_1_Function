42099ea9490700c0075816b775e1ae5b21f95f2c
alexandr-prokhorov <prohorovalex0013@gmail.ru>
2024-11-04 21:43:53 +0400

    Добавил комментарий. Add commentary.

--- modul_4_1/task_1.py
+++ modul_4_1/task_1.py
@@ -9,6 +9,7 @@ def display_text():
     name_autor = "Bill Gates"
     print("“Don't compare yourself with anyone in this world…")
     print("if you do so, you are insulting yourself.”")
+    #Сделал отступ для оформления как в задании.
     print(f"{name_autor:>50}")
 
 display_text()
