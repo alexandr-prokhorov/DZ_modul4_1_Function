2d9cc4a92b21f278d05b8ca17e451bd6415a0db8
alexandr-prokhorov <prohorovalex0013@gmail.ru>
2024-11-07 22:18:32 +0400

    Исправил неточности.Corrected inaccuracies

--- DZ_modul_4_2/main.py
+++ DZ_modul_4_2/main.py
@@ -1,4 +1,4 @@
-from modules.module_1 import info_modul_1
+from modules.module_1 import info_module_1
 from modules.module_2 import info_module_2
 
 from mypackage.folder_1.file_1 import info_file_1
@@ -8,7 +8,7 @@ from mypackage.folder_2.file_4 import info_file_4
 from mypackage.folder_1.folder_1_1.file_1_1 import info_file_1_1
 from mypackage.folder_1.folder_1_1.file_2_2 import info_file_2_2
 
-info_modul_1()
+info_module_1()
 info_module_2()
 info_file_1()
 info_file_2()
--- DZ_modul_4_2/modules/module_1.py
+++ DZ_modul_4_2/modules/module_1.py
@@ -1,2 +1,2 @@
-def info_modul_1():
+def info_module_1():
     print("Функция импортирована из модуля module_1 в папке modules")
