8a0459a0f69aab21a5ca682c3a7e8d4938edc63e
alexandr-prokhorov <prohorovalex0013@gmail.ru>
2024-11-10 21:31:58 +0400

    Исправил код криво работала проверка на уникальность.I fixed the code, the uniqueness check worked crookedly

--- DZ_modul_4_3/main.py
+++ DZ_modul_4_3/main.py
@@ -7,44 +7,58 @@ patterns = get_reg_data()
 while len(users_list) < 3:
     user_data_list = []
 
-    while len(user_data_list) < 1:
+    while True:
         input_name = input("Введите имя пользователя: ").title().strip()
         name = reg_check(input_name, patterns["name"], users_list)
         if name:
             user_data_list.append(name)
             print("Имя принято")
+            break
         else:
-            print("Ошибка: содержит не подходящие символы")
+            print("Ошибка: содержит неподходящие символы или имя уже существует.")
 
-    while len(user_data_list) < 2:
+    while True:
         input_surname = input("Введите фамилию пользователя: ").title().strip()
         surname = reg_check(input_surname, patterns["surname"], users_list)
         if surname:
             user_data_list.append(surname)
             print("Фамилия принята")
+            break
         else:
-            print("Ошибка: содержит не подходящие символы")
+            print("Ошибка: содержит неподходящие символы или фамилия уже существует.")
 
-    while len(user_data_list) < 3:
+    while True:
         input_telephone = input("Введите Ваш номер телефона в формате +***(**)*******: ").strip()
-        telephone = reg_check(input_telephone, patterns["telephone"], users_list)
+        telephone = reg_check(input_telephone, patterns["telephone"], users_list, 2)
         if telephone:
             user_data_list.append(telephone)
             print("Телефон принят")
+            break
         else:
-            print("Телефон не соответствует формату +***(**)*******")
-    while len(user_data_list) < 4:
-        input_mail = input("Ведите ваш email на яндексе: ").strip()
-        mail = reg_check(input_mail, patterns["mail"], users_list)
+            print("Телефон не соответствует формату или уже существует.")
+
+    while True:
+        input_mail = input("Введите ваш email на Яндексе: ").strip()
+        mail = reg_check(input_mail, patterns["mail"], users_list, 3)
         if mail:
             user_data_list.append(mail)
             print("Почта принята")
+            break
         else:
-            print("Почта должна содержать @yandex.ru")
+            print("Почта должна содержать @yandex.ru или уже существует.")
 
-    users_list.append(user_data_list)
+    user_duplicate = any(
+        user[2] == user_data_list[2] or
+        user[3] == user_data_list[3]
+        for user in users_list
+    )
+
+    if user_duplicate:
+        print("Ошибка: пользователь с такими данными уже существует. Попробуйте снова.")
+    else:
+        users_list.append(user_data_list)
+        print("Пользователь добавлен.")
 
 print("Готовый список пользователей: ")
-
 for user in users_list:
     print(user)
--- DZ_modul_4_3/utils.py
+++ DZ_modul_4_3/utils.py
@@ -10,16 +10,14 @@ def get_reg_data():
     return patterns
 
 def check_unique_data(user_data, data_to_check):
-    for user in data_to_check:
-        if user_data == user:
-            return False
-    return True
+    return user_data not in data_to_check
 
-def reg_check(user_data, reg_pattern, data_to_check=None):
+def reg_check(user_data, reg_pattern, users_list, field_index=None):
     if not re.match(reg_pattern, user_data):
         return None
 
-    if data_to_check is not None:
+    if field_index is not None:
+        data_to_check = [user[field_index] for user in users_list]
         if not check_unique_data(user_data, data_to_check):
             return None
     return user_data
